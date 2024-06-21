35c33a5c93e0deafd02d4f76e6eb34d4b0a11420
Marinalm15 <marinalm15@hotmail.com>
2024-06-20 21:24:30 -0300

    add images

---
+++ src/helpers/Staff/equipa.ts
@@ -0,0 +1,429 @@
+import anaCabral from "../../assets/Ana-Cabral.jpg"
+import manuelSa from "../../assets/Manuel-Sá-Paula.jpg"
+import anaLomba from "../../assets/Ana-Lomba.jpg"
+import arlinda from "../../assets/Arlinda-Oliveira.jpg"
+import catarinaMoreira from "../../assets/Catarina-Moreira.jpg"
+import claraMiranda from "../../assets/Clara-Miranda.jpg"
+import claudiaAlves from "../../assets/Cláudia-Alves.jpg"
+import danielaSilva from "../../assets/Daniela-Silva.jpg"
+import deolindaSilva from "../../assets/Deolinda-Silva.jpg"
+import dianaMartins from "../../assets/Diana.jpg"
+import helenaCardoso from "../../assets/Helena-Cardoso.jpg"
+import inesOliveira from "../../assets/Inês-Oliveira.jpg"
+import isabelOliveira from "../../assets/Isabel-Oliveira.jpg"
+import juliaLomba from "../../assets/Júlia-Lomba.jpg"
+import leandraSa from "../../assets/Leandra-Sá.jpg"
+import LurdesVale from "../../assets/Lurdes-Vale.jpg"
+import mariaLemos from "../../assets/Maria-Lemos.jpg"
+import RitaGomes from "../../assets/Rita-Gomes.jpg"
+import silviaCerqueira from "../../assets/Sílvia-Cerqueira.jpg"
+import semFoto from "../../assets/sem_foto.jpg"
+
+
+export const direcao = [
+    {
+        id: 1,
+        name: "Manuel Sá Paula",
+        cargo: "Diretor",
+        img: manuelSa
+    },
+    {
+        id: 2,
+        name: "Ana Cabral",
+        cargo: "Diretora Adjunta",
+        img: anaCabral,
+    }
+]
+
+export const adiministrativo = [
+    {
+        id: 1,
+        name: "Sara Mano",
+        cargo: "Secretária",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Helena Terroso",
+        cargo: "Escriturária",
+        img: ""
+    },
+    {
+        id: 3,
+        name: "Joana Sousa",
+        cargo: "Secretária",
+        img: ""
+    }
+]
+
+export const portaria = [
+    {
+        id: 1,
+        name: "Ana Serre",
+        cargo: "Rececionista",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Piedade Martins",
+        cargo: "Rececionista",
+        img: ""
+    }
+]
+
+export const motoristas = [
+    {
+        id: 1,
+        name: "Carmo Vilas Boas",
+        cargo: "Motorista",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Hugo Silva",
+        cargo: "Motorista",
+        img: ""
+    }
+]
+
+export const dispensa = [
+    {
+        id: 1,
+        name: "Fábio Valente",
+        cargo: "Despenseiro",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Filomena Costa",
+        cargo: "Despenseiro",
+        img: ""
+    },
+    {
+        id: 3,
+        name: "Paula Ferreira",
+        cargo: "Despenseiro",
+        img: ""
+    }
+]
+
+export const gerais = [
+    {
+        id: 1,
+        name: "Céu Gomes",
+        cargo: "Trab. Auxiliar",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Judit Pereira",
+        cargo: "Trab. Auxiliar",
+        img: ""
+    },
+    {
+        id: 3,
+        name: "Rosa Araújo",
+        cargo: "Jardineira",
+        img: ""
+    }
+]
+
+export const lavandaria = [
+    {
+        id: 1,
+        name: "Conceição Alves",
+        cargo: "Roupeira",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Sandra Fernandes",
+        cargo: "Educadora Social",
+        img: ""
+    }
+]
+
+export const cozinha = [
+    {
+        id: 1,
+        name: "Celina Brito",
+        cargo: "Cozinheira",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Cândida Rego",
+        cargo: "Cozinheira",
+        img: ""
+    },
+    {
+        id: 3,
+        name: "Emília Lamela",
+        cargo: "Cozinheira",
+        img: ""
+    },
+    {
+        id: 4,
+        name: "Fátima Carvalho",
+        cargo: "Cozinheira",
+        img: ""
+    },
+    {
+        id: 5,
+        name: "Lúcia Pombo",
+        cargo: "Cozinheira",
+        img: ""
+    }
+]
+
+export const creche = [
+    {
+        id: 1,
+        name: "Ana Lomba",
+        cargo: "Auxiliar de Ação Educativa",
+        img: anaLomba
+    },
+    {
+        id: 2,
+        name: "Arlinda Oliveira",
+        cargo: "Auxiliar de Ação Educativa",
+        img: arlinda
+    },
+    {
+        id: 3,
+        name: "Catarina Moreira",
+        cargo: "Auxiliar de Ação Educativa",
+        img: catarinaMoreira
+    },
+    {
+        id: 4,
+        name: "Clara Miranda",
+        cargo: "Auxiliar de Ação Educativa",
+        img: claraMiranda
+    },
+    {
+        id: 5,
+        name: "Cláudia Alves",
+        cargo: "Auxiliar de Ação Educativa",
+        img: claudiaAlves
+    },
+    {
+        id: 6,
+        name: "Daniela Silva",
+        cargo: "Auxiliar de Ação Educativa",
+        img: danielaSilva
+    },
+    {
+        id: 7,
+        name: "Deolinda Silva",
+        cargo: "Auxiliar de Ação Educativa",
+        img: deolindaSilva
+    },
+    {
+        id: 8,
+        name: "Diana Martins",
+        cargo: "Auxiliar de Ação Educativa",
+        img: dianaMartins
+    },
+    {
+        id: 9,
+        name: "Helena Cardoso",
+        cargo: "Auxiliar de Ação Educativa",
+        img: helenaCardoso
+    },
+    {
+        id: 10,
+        name: "Inês Oliveira",
+        cargo: "Auxiliar de Ação Educativa",
+        img: inesOliveira
+    },
+    {
+        id: 11,
+        name: "Isabel Oliveira",
+        cargo: "Auxiliar de Ação Educativa",
+        img: isabelOliveira
+    },
+    {
+        id: 12,
+        name: "Júlia Lomba",
+        cargo: "Educadora",
+        img: juliaLomba
+    },
+    {
+        id: 13,
+        name: "Leandra Sá",
+        cargo: "Educadora",
+        img: leandraSa
+    },
+    {
+        id: 14,
+        name: "Lurdes Vale",
+        cargo: "Auxiliar de Ação Educativa",
+        img: LurdesVale
+    },
+    {
+        id: 15,
+        name: "Maria Lemos",
+        cargo: "Auxiliar de Ação Educativa",
+        img: mariaLemos
+    },
+    {
+        id: 16,
+        name: "Rita Gomes",
+        cargo: "Auxiliar de Ação Educativa",
+        img: RitaGomes
+    },
+    {
+        id: 17,
+        name: "Sílvia Cerqueira",
+        cargo: "Educadora",
+        img: silviaCerqueira
+    },
+    {
+        id: 18,
+        name: "Ana Ferreira",
+        cargo: "Trab. Auxiliar",
+        img: semFoto
+    },
+    {
+        id: 19,
+        name: "Fátima Barbosa",
+        cargo: "Auxiliar de Ação Educativa",
+        img: semFoto
+    },
+    {
+        id: 20,
+        name: "Rafaela Rodrigues",
+        cargo: "Auxiliar de Ação Educativa",
+        img: semFoto
+    }
+]
+
+export const preEscola = [
+    {
+        id: 1,
+        name: "Amélia Silva",
+        cargo: "Educadora",
+        img: ""
+    },
+    {
+        id: 2,
+        name: "Ana Costa",
+        cargo: "Auxiliar de Ação Educativa",
+        img: ""
+    },
+    {
+        id: 3,
+        name: "Célia Lopes",
+        cargo: "Educadora",
+        img: ""
+    },
+    {
+        id: 4,
+        name: "Clara Miranda",
+        cargo: "Auxiliar de Ação Educativa",
+        img: ""
+    },
+    {
+        id: 5,
+        name: "Céu Silva",
+        cargo: "Trab. Auxiliar",
+        img: ""
+    },
+    {
+        id: 6,
+        name: "Conceição Sá",
+        cargo: "Educadora",
+        img: ""
+    },
+    {
+        id: 7,
+        name: "Delfim Miranda",
+        cargo: "Trab. Auxiliar",
+        img: ""
+    },
+    {
+        id: 8,
+        name: "Emília Soutinho",
+        cargo: "Trab. Auxiliar",
+        img: ""
+    },
+    {
+        id: 9,
+        name: "Isabel Figueiredo",
+        cargo: "Trab. Auxiliar",
+        img: ""
+    },
+    {
+        id: 10,
+        name: "Isaura Chaves",
+        cargo: "Educadora",
+        img: ""
+    },
+    {
+        id: 11,
+        name: "Isilda Afonseca",
+        cargo: "Auxiliar de Ação Educativa",
+        img: ""
+    },
+    {
+        id: 12,
+        name: "Liliana Pereira",
+        cargo: "Educadora",
+        img: ""
+    },
+    {
+        id: 13,
+        name: "Lurdes Capitão",
+        cargo: "Auxiliar de Ação Educativa",
+        img: ""
+    },
+    {
+        id: 14,
+        name: "Madalena Figueiredo",
+        cargo: "Auxiliar de Ação Educativa",
+        img: ""
+    },
+    {
+        id: 15,
+        name: "Marta Araújo",
+        cargo: "Auxiliar de Ação Educativa",
+        img: ""
+    },
+    {
+        id: 16,
+        name: "Raquel Silva",
+        cargo: "Auxiliar de Ação Educativa",
+        img: ""
+    },
+    {
+        id: 17,
+        name: "Sandra Alves",
+        cargo: "Educadora",
+        img: ""
+    },
+    {
+        id: 18,
+        name: "Sónia Faria",
+        cargo: "Educadora",
+        img: ""
+    },
+    {
+        id: 19,
+        name: "Ana Vieira",
+        cargo: "Auxiliar de Ação Educativa",
+        img: semFoto
+    },
+    {
+        id: 20,
+        name: "Ana Rita Pereira",
+        cargo: "Auxiliar de Ação Educativa",
+        img: semFoto
+    },
+    {
+        id: 21,
+        name: "Carmo Cordeiro",
+        cargo: "Auxiliar de Ação Educativa",
+        img: semFoto
+    },
+]
