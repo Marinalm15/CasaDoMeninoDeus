c87e02f97a8790fc1d09fb08076e3e0c13da5480
Marinalm15 <marinalm15@hotmail.com>
2024-06-23 22:08:42 -0300

    helpers with infos

---
+++ src/helpers/departamentos.ts
@@ -0,0 +1,50 @@
+export const departamentos = [
+    {
+        id: "direcao",
+        name: "Direção",
+    },
+    {
+        id: "adiministrativo",
+        name: "Serviços Administrativos",
+    },
+    {
+        id: "portaria",
+        name: "Portaria",
+    },
+    {
+        id: "motoristas",
+        name: "Motoristas",
+    },
+    {
+        id: "despensa",
+        name: "Despensa",
+    },
+    {
+        id: "servicos_gerais",
+        name: "Serviços Gerais",
+    },
+    {
+        id: "lavandaria",
+        name: "Lavandaria",
+    },
+    {
+        id: "",
+        name: "Cozinha",
+    },
+    {
+        id: "creche",
+        name: "Creche",
+    },
+    {
+        id: "pre_escola",
+        name: "Ensino Pré-Escolar",
+    },
+    {
+        id: "1_ciclo",
+        name: "1º Ciclo",
+    },
+    {
+        id: "casa_acolhimento",
+        name: "Casa de Acolhimento",
+    },
+]
